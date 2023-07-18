ddc79fdeaf8d397a18a829bbb2988af465960dac
nicoandrade50 <83848730+nicoandrade50@users.noreply.github.com>
2023-07-18 10:48:38 -0500

    Update suma.js

--- suma.js
+++ suma.js
@@ -1,1 +1,21 @@
+function Punto(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
+
+  Punto.prototype.suma = function(punto) {
+    const sumaX = this.x + punto.x;
+    const sumaY = this.y + punto.y;
+    return new Punto(sumaX, sumaY);
+  };
+
+
+  Punto.prototype.toString = function() {
+    return `(${this.x},${this.y})`;
+  };
+
+
+  let p1 = new Punto(1, 2);
+  let p2 = new Punto(2, 3);
+  console.log(p1.suma(p2).toString());
